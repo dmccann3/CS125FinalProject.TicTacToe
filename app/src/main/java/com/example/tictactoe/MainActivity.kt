9c5a37add9378b689cda486431ec2d995673aebc
claireyip <ynyip2@illinois.edu>
2020-04-24 09:32:16 +0800

    Start Button / Enter game

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     *
+     */
+    fun startGame(view: View) {
+        val intent = Intent(this, GameActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
